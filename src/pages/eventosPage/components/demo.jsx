bd3eea686cc1224e9d99b144e702dfe6fa39d74c
Matias Azcui <matias.azcui@gmail.com>
2022-03-26 10:34:57 -0300

    last commit

--- src/pages/eventosPage/components/demo.jsx
+++ src/pages/eventosPage/components/demo.jsx
@@ -60,48 +60,6 @@ class Demo extends React.Component {
           onAppointmentUpdated={false} //this.showUpdatedToast
           onAppointmentDeleted={this.showDeletedToast}
         />
-        {/* <div className="options">
-          <div className="caption">Options</div>
-          <div className="options-container">
-            <div className="option">
-              <CheckBox
-                defaultValue={this.state.allowAdding}
-                text="Allow adding"
-                onValueChanged={this.onAllowAddingChanged}
-              />
-            </div>
-            <div className="option">
-              <CheckBox
-                defaultValue={this.state.allowDeleting}
-                text="Allow deleting"
-                onValueChanged={this.onAllowDeletingChanged}
-              />
-            </div>
-            <div className="option">
-              <CheckBox
-                defaultValue={this.state.allowUpdating}
-                text="Allow updating"
-                onValueChanged={this.onAllowUpdatingChanged}
-              />
-            </div>
-            <div className="option">
-              <CheckBox
-                defaultValue={this.state.allowResizing}
-                text="Allow resizing"
-                onValueChanged={this.onAllowResizingChanged}
-                disabled={!this.state.allowUpdating}
-              />
-            </div>
-            <div className="option">
-              <CheckBox
-                defaultValue={this.state.allowDragging}
-                text="Allow dragging"
-                onValueChanged={this.onAllowDraggingChanged}
-                disabled={!this.state.allowUpdating}
-              />
-            </div>
-          </div>
-        </div> */}
       </React.Fragment>
     );
   }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 const baseUrl = process.env.REACT_APP_API_URL;
 
 export const userService = {
